86430a03313d225095cfb9e42f3ba371e77764b7
tushit24 <rishitiwariofficial@gmail.com>
2025-08-05 20:46:27 +0530

    New Question Added

---
+++ 0347-Top K Frequent Elements/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+    List<Integer>[] bucket = new List[nums.length + 1];
+    Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+    for (int n : nums) {
+      frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+    }
+
+    for (int key : frequencyMap.keySet()) {
+      int frequency = frequencyMap.get(key);
+      if (bucket[frequency] == null) {
+        bucket[frequency] = new ArrayList<>();
+      }
+      bucket[frequency].add(key);
+    }
+
+    List<Integer> topK = new ArrayList<>();
+    for (int pos = bucket.length - 1;pos >= 0 && topK.size() < k; pos--) {
+      if (bucket[pos] != null) {
+        topK.addAll(bucket[pos]);
+      }
+    }
+
+    return topK.stream().mapToInt(i -> i).toArray();
+ 
+    }
+}
